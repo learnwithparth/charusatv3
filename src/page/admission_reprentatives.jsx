8f9779f249395965253594eaa4e856f01b8a5476
Ravi Patel <ravipatel.it@charusat.ac.in>
2022-12-10 17:22:56 +0530

    boxes added

--- src/page/admission_reprentatives.jsx
+++ src/page/admission_reprentatives.jsx
@@ -20,6 +20,7 @@ const Admission_Reprentatives = () => {
                         <div className="post-inner">
                           <div className="post-content">
                             <h3>Non-credit Course & Certification Course</h3>
+
                             <p>
                               Lorem ipsum dolor sit amet, consectetur
                               adipisicing elit. Pariatur quas ratione quae unde
@@ -28,6 +29,7 @@ const Admission_Reprentatives = () => {
                               voluptates aperiam iusto architecto perspiciatis.
                             </p>
                             <br />
+
                             <Container>
                               <Row className="p-2">
                                 <Col>
@@ -88,6 +90,229 @@ const Admission_Reprentatives = () => {
                                     </p>
                                   </div>
                                 </Col>
+                              </Row>
+                              <Row className="p-2">
+                                <Col>
+                                  <div className="card p-2">
+                                    <h6>
+                                      Chandubhai S. Patel Institute Of
+                                      <br />
+                                      Technology
+                                    </h6>
+                                    <p>Information Technology</p>
+                                    <p>
+                                      Ms Rekha Karangiya, Assistant Professor
+                                    </p>
+                                    <p>
+                                      <i className="icofont-email"></i>{" "}
+                                      admission_cspit_it@charusat.ac.in
+                                    </p>
+                                    <p>
+                                      <i className="icofont-phone"></i>{" "}
+                                      +91-9265264958
+                                    </p>
+                                  </div>
+                                </Col>
+                                <Col>
+                                  <div className="card p-2">
+                                    <h6>
+                                      Chandubhai S. Patel Institute Of <br />{" "}
+                                      Technology
+                                    </h6>
+                                    <p>Electronics & Communication Engg.</p>
+                                    <p>Ms Poonam Thanki, Assistant Professor</p>
+                                    <p>
+                                      <i className="icofont-email"></i>{" "}
+                                      admission_cspit_ec@charusat.ac.in
+                                    </p>
+                                    <p>
+                                      <i className="icofont-phone"></i>{" "}
+                                      +91-9978992692
+                                    </p>
+                                  </div>
+                                </Col>
+                                <Col>
+                                  <div className="card p-2">
+                                    <h6>
+                                      Chandubhai S. Patel Institute Of
+                                      <br />
+                                      Technology
+                                    </h6>
+                                    <p>Electrical Engineering</p>
+                                    <p>Mr. Pratik Mochi, Assistant Professor</p>
+                                    <p>
+                                      <i className="icofont-email"></i>{" "}
+                                      admission_cspit_ee@charusat.ac.in
+                                    </p>
+                                    <p>
+                                      <i className="icofont-phone"></i>{" "}
+                                      +91-9974916313
+                                    </p>
+                                  </div>
+                                </Col>
+                              </Row>
+                              <Row className="p-2">
+                                <Col>
+                                  <div className="card p-2">
+                                    <h6>
+                                      Chandubhai S. Patel Institute Of
+                                      <br />
+                                      Technology
+                                    </h6>
+                                    <p>Civil Engineering</p>
+                                    <p>
+                                      Mr. Ankit Wankawala, Assistant Professor
+                                    </p>
+                                    <p>
+                                      <i className="icofont-email"></i>{" "}
+                                      admission_cspit_cl@charusat.ac.in
+                                    </p>
+                                    <p>
+                                      <i className="icofont-phone"></i>{" "}
+                                      +91-9033108890
+                                    </p>
+                                  </div>
+                                </Col>
+                                <Col>
+                                  <div className="card p-2">
+                                    <h6>
+                                      Chandubhai S. Patel Institute Of <br />{" "}
+                                      Technology
+                                    </h6>
+                                    <p>Mechanical Engineering</p>
+                                    <p>
+                                      Mr. Sagar Chokshi, Assistant Professor
+                                    </p>
+                                    <p>
+                                      <i className="icofont-email"></i>{" "}
+                                      admission_cspit_me@charusat.ac.in
+                                    </p>
+                                    <p>
+                                      <i className="icofont-phone"></i>{" "}
+                                      +91-9726622007
+                                    </p>
+                                  </div>
+                                </Col>
+                                <Col>
+                                  <div className="card p-2">
+                                    <h6>
+                                      Charotar Institute of
+                                      <br />
+                                      Paramedical Sciences
+                                    </h6>
+                                    <p>B Sc & M Sc</p>
+                                    <p>
+                                      Mr. Yogesh Chauhan, Assistant Professor
+                                    </p>
+                                    <p>
+                                      <i className="icofont-email"></i>{" "}
+                                      admission_cips@charusat.ac.in
+                                    </p>
+                                    <p>
+                                      <i className="icofont-phone"></i>{" "}
+                                      +91-9327064424
+                                    </p>
+                                  </div>
+                                </Col>
+                              </Row>
+                              <Row className="p-2">
+                                <Col>
+                                  <div className="card p-2">
+                                    <h6>
+                                      Devang Patel Institue of Advance
+                                      Technology and Research
+                                    </h6>
+                                    <p>CSE / IT / CE Engineering</p>
+                                    <p>
+                                      Mr. Hardik Jayswal, Assistant Professor
+                                    </p>
+                                    <p>
+                                      <i className="icofont-email"></i>{" "}
+                                      admission_depstar@charusat.ac.in
+                                    </p>
+                                    <p>
+                                      <i className="icofont-phone"></i>{" "}
+                                      +91-9825961830
+                                    </p>
+                                  </div>
+                                </Col>
+                                <Col>
+                                  <div className="card p-2">
+                                    <h6>
+                                      Indukaka Ipcowala Institute of
+                                      <br /> Management
+                                    </h6>
+                                    <p>BBA / MBA / PGDM</p>
+                                    <p>Dr. Pramod Patel, Librarian</p>
+                                    <p>
+                                      <i className="icofont-email"></i>{" "}
+                                      admission_i2im@charusat.ac.in
+                                    </p>
+                                    <p>
+                                      <i className="icofont-phone"></i>{" "}
+                                      +91-9879334259
+                                    </p>
+                                  </div>
+                                </Col>
+                                <Col>
+                                  <div className="card p-2">
+                                    <h6>
+                                      Manikaka Topawala Institute of <br />{" "}
+                                      Nursing
+                                    </h6>
+                                    <p>GNM / B Sc(Nursing) / M Sc(Nursing)</p>
+                                    <p>
+                                      Mr. Jamin Chauhan, Assistant Professor
+                                    </p>
+                                    <p>
+                                      <i className="icofont-email"></i>{" "}
+                                      admission_mtin@charusat.ac.in
+                                    </p>
+                                    <p>
+                                      <i className="icofont-phone"></i>{" "}
+                                      +91-9624569589
+                                    </p>
+                                  </div>
+                                </Col>
+                              </Row>
+                              <Row className="p-2">
+                                <Col>
+                                  <div className="card p-2">
+                                    <h6>
+                                      P. D. Patel Institute of <br />
+                                      Applied Science
+                                    </h6>
+                                    <p>B Sc & M Sc</p>
+                                    <p>Mr. Rakesh Dave, Assistant Registrar</p>
+                                    <p>
+                                      <i className="icofont-email"></i>{" "}
+                                      admission_pdpias@charusat.ac.in
+                                    </p>
+                                    <p>
+                                      <i className="icofont-phone"></i>{" "}
+                                      +91-9825964854
+                                    </p>
+                                  </div>
+                                </Col>
+                                <Col>
+                                  <div className="card p-2">
+                                    <h6>
+                                      Ramanbhai Patel College of <br />
+                                      Pharmacy
+                                    </h6>
+                                    <p>B.Pharm. / M.Pharm.</p>
+                                    <p>Dr. Ashish Patel, Assistant Professor</p>
+                                    <p>
+                                      <i className="icofont-email"></i>{" "}
+                                      admission_rpcp@charusat.ac.in
+                                    </p>
+                                    <p>
+                                      <i className="icofont-phone"></i>{" "}
+                                      +91-9879825183
+                                    </p>
+                                  </div>
+                                </Col>
+                                <Col>{""}</Col>
                               </Row>
                             </Container>
                           </div>
