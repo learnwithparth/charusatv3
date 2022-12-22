a5188f24ad69249fba80c27f0de3342fea99bfa5
Jalpeshv <jalpeshvasa.it@charusat.ac.in>
2022-12-22 14:01:58 +0530

    wellness program

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ import IPR from "./page/ipr"
 import CDPC from "./page/cdpc";
 import Ethics from "./page/ethics";
 import Accreditation from "./page/Accreditation";
+import WellnessProgram from "./page/wellnessProgram";
+
 
 function App() {
 	return (
@@ -32,6 +34,7 @@ function App() {
 			<Route path="/ethics" element={<Ethics/>}/>
 			<Route path="*" element={<ErrorPage />} />
 			<Route path="/accreditation" element={<Accreditation />} />
+			<Route path="/wellness_Program" element={<WellnessProgram />} />
 
 			</Routes>
 			<Footer/>
