dc94b096c1af9b9ee83db0521dc56b022d743177
ravichanga101 <ravifromchanga@gmail.com>
2023-03-23 16:13:30 +0530

    changed slider

--- src/component/section/banner-5.jsx
+++ src/component/section/banner-5.jsx
@@ -6,6 +6,10 @@ import Slider from "react-animated-slider";
 import "react-animated-slider/build/horizontal.css";
 import "../../assets/css/slider-animations.css";
 import "../../assets/css/slider-banner.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+
+import "swiper/swiper-bundle.min.css";
 
 const slides = [
   { title: "First item", description: "Lorem ipsum" },
@@ -17,7 +21,7 @@ const content = [
     title: "NAAC 'A+' University ",
     description: "Take a tour",
     button: "Apply Now",
-    image: "http://172.16.102.120:9090/upload/images/home/s7.webp",
+    image: "assets/images/home/s7.webp",
     user: "Luan Gjokaj",
     userProfile: "https://i.imgur.com/JSW6mEk.png",
   },
@@ -26,7 +30,7 @@ const content = [
     description:
       "Highly equipped and advanced labs for practical learning and in-depth research",
     button: "Apply Now",
-    image: "http://172.16.102.120:9090/upload/images/home/s8.webp",
+    image: "assets/images/home/s8.webp",
     user: "Erich Behrens",
     userProfile: "https://i.imgur.com/0Clfnu7.png",
   },
@@ -35,7 +39,7 @@ const content = [
     description:
       "Our hostels give out the vibes of comfort and contentment, just like home",
     button: "Apply Now",
-    image: "http://172.16.102.120:9090/upload/images/home/s3.webp",
+    image: "assets/images/home/s3.webp",
     user: "Bruno Vizovskyy",
     userProfile: "https://i.imgur.com/4KeKvtH.png",
   },
@@ -53,7 +57,7 @@ const btnText = "View Lincon’s Courses";
 const BannerFive = () => {
   return (
     <section className="d-block clearfix">
-      <Slider className="slider-wrapper" autoplay="4000">
+      {/* <Slider className="slider-wrapper" autoplay="4000">
         {content.map((item, index) => (
           <div
             key={index}
@@ -69,7 +73,7 @@ const BannerFive = () => {
               <br /> <br /> <br /> <br />
               <br />
               <div className="subTitle-slider">{item.description}</div>
-              {/* <button>{item.button}</button> */}
+              <button>{item.button}</button>
             </div>
             <section>
               <img src={item.userProfile} alt={item.user} />
@@ -79,7 +83,35 @@ const BannerFive = () => {
             </section>
           </div>
         ))}
-      </Slider>
+      </Slider> */}
+      <Swiper slidesPerView={1} modules={[Navigation]} navigation>
+        {content.map((item, index) => (
+          <SwiperSlide
+            className="slider-content"
+            style={{
+              height: "800px",
+              width: "100%",
+              // background: `url('${item.image}') no-repeat center center`,
+            }}
+            key={item.index}
+          >
+            <img
+              src={item.image}
+              alt=""
+              style={{ width: "100%", height: "100%", objectFit: "cover" }}
+            />
+            <div className="inner">
+              <div className="h1 main-text-slider animated-text-front-page">
+                {item.title}
+              </div>
+              <br /> <br /> <br /> <br />
+              <br />
+              <div className="subTitle-slider">{item.description}</div>
+              {/* <button>{item.button}</button> */}
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
       <br />
       {/* <p className="text-center mt-1">
         <span
