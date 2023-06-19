50f4b49a0571161aeaba4fd2104ea742aca367f6
Jay Keraliya <jaykeraliya0@gmail.com>
2023-06-19 13:55:09 +0530

    CPSH Page changed

--- src/page/CPSH.jsx
+++ src/page/CPSH.jsx
@@ -18,43 +18,92 @@ const CPSH = () => {
                       <div className="post-item style-2">
                         <div className="post-inner">
                           <div className="post-content">
-                            <h3>Cell for Prevention of Sexual Harassment (CPSH)</h3>
-                            <div className="meta-post" >
+                            <h3>
+                              Cell for Prevention of Sexual Harassment (CPSH)
+                            </h3>
+                            <div className="meta-post">
                               <h4>Objective</h4>
                               The objectives of the Cell and its policy are:
                               <ul>
                                 <li>
-                                  To create a healthy working environment for both the genders by establishing guidelines to deter any Sexual Harassment
+                                  To create a healthy working environment for
+                                  both the genders by establishing guidelines to
+                                  deter any Sexual Harassment
                                 </li>
                                 <li>
-                                  To encourage raising of awareness about gender sensitivity and gender equality at campus
+                                  To encourage raising of awareness about gender
+                                  sensitivity and gender equality at campus
                                 </li>
                                 <li>
-                                  To set a process of accepting grievances on sexual harassment, gender dignity, their fair investigation and appropriate action on the findings of the enquiries
+                                  To set a process of accepting grievances on
+                                  sexual harassment, gender dignity, their fair
+                                  investigation and appropriate action on the
+                                  findings of the enquiries
                                 </li>
                               </ul>
                             </div>
-                            <div className="meta-post" >
+                            <div className="meta-post">
                               <h4>Definition</h4>
-                              Sexual Harassment includes such unwelcome sexually determined behavior by any person individually or in association with other persons or by any person in authority, whether either directly or by implications, such as following:
-                              <br />
-                              {/* <ul> */}
-                               <li>Eve teasing</li> 
-                               <li>Jokes causing or likely to cause awkwardness or embarrassment</li>
-                               <li>Unsavory remarks </li>
-                               <li>Gender Based insults or sexiest remarks </li>
-                               <li>Innuendos and taunts </li>
-                               <li>Unwelcome sexual overtone in any manner such as over telephone (Obnoxious telephone calls) and the like </li>
-                               <li>Touching or brushing against any part of the body and the like </li>
-                               <li>Display of pornographic or other offensive or derogatory pictures, cartoons pamphlets or sayings </li>
-                               <li>Forcible physical touch or molestation </li>
-                               <li>Physical confinement against one’s will and any other act likely to violate one’s privacy </li>
-                               <li>Unwelcomed physical contacts and advances </li>
-                               <li>A communication for sexual favors </li>
-                               <li>Any other unwelcome, physical, verbal or non-verbal conduct of sexual nature directly, indirectly, or through electronic media like SMS, MMS, Social Networking sites etc. </li>
-                               <li>Any act of Gender Discrimination such as denial of employment to women due to women’s reproductive role, or rigidity in attitude, which hinders the reproductive role of women (for example, non-flexible work hours and timings, non-availability of day care services, lack of maternity leave, etc). </li>
-                              {/* </ul> */}
-
+                              Sexual Harassment includes such unwelcome sexually
+                              determined behavior by any person individually or
+                              in association with other persons or by any person
+                              in authority, whether either directly or by
+                              implications, such as following:
+                              <ul
+                                style={{
+                                  display: "flex",
+                                  flexDirection: "column",
+                                  alignItems: "flex-start",
+                                  justifyContent: "flex-start",
+                                }}
+                              >
+                                <li>Eve teasing</li>
+                                <li>
+                                  Jokes causing or likely to cause awkwardness
+                                  or embarrassment
+                                </li>
+                                <li>Unsavory remarks </li>
+                                <li>Gender Based insults or sexiest remarks</li>
+                                <li>Innuendos and taunts </li>
+                                <li>
+                                  Unwelcome sexual overtone in any manner such
+                                  as over telephone (Obnoxious telephone calls)
+                                  and the like
+                                </li>
+                                <li>
+                                  Touching or brushing against any part of the
+                                  body and the like
+                                </li>
+                                <li>
+                                  Display of pornographic or other offensive or
+                                  derogatory pictures, cartoons pamphlets or
+                                  sayings
+                                </li>
+                                <li>Forcible physical touch or molestation </li>
+                                <li>
+                                  Physical confinement against one’s will and
+                                  any other act likely to violate one’s privacy
+                                </li>
+                                <li>
+                                  Unwelcomed physical contacts and advances
+                                </li>
+                                <li>A communication for sexual favors </li>
+                                <li>
+                                  Any other unwelcome, physical, verbal or
+                                  non-verbal conduct of sexual nature directly,
+                                  indirectly, or through electronic media like
+                                  SMS, MMS, Social Networking sites etc.
+                                </li>
+                                <li>
+                                  Any act of Gender Discrimination such as
+                                  denial of employment to women due to women’s
+                                  reproductive role, or rigidity in attitude,
+                                  which hinders the reproductive role of women
+                                  (for example, non-flexible work hours and
+                                  timings, non-availability of day care
+                                  services, lack of maternity leave, etc).
+                                </li>
+                              </ul>
                             </div>
                           </div>
                         </div>
@@ -74,6 +123,6 @@ const CPSH = () => {
       </div>
     </Fragment>
   );
-}
+};
 
 export default CPSH;
