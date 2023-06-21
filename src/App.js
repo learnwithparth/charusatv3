4bd6fa47e8c2990c283afba7635a3962ed59614c
Jay Keraliya <jaykeraliya0@gmail.com>
2023-06-21 10:41:34 +0530

    created IEC Page

--- src/App.js
+++ src/App.js
@@ -85,6 +85,7 @@ import ECC from "./page/ECC";
 import EOC from "./page/EOC";
 import WDC from "./page/WDC";
 import DvvClarifications from "./page/dvv_clarifications";
+import IEC from "./page/IEC";
 
 function App() {
   // const basename = document.querySelector('base')?.getAttribute('href') ?? '/'    ;
@@ -170,6 +171,7 @@ function App() {
         <Route path="/ecc" element={<ECC />} />
         <Route path="/eoc" element={<EOC />} />
         <Route path="/wdc" element={<WDC />} />
+        <Route path="/IEC" element={<IEC />} />
         {/* http://preview.themeforest.net/item/edukon-education-and-lms-react-js-template/full_screen_preview/38286924?_ga=2.76967702.264634929.1671012406-1346785869.1667801964 */}
         {/* <Route path="/announcements" element={<Announcements />} /> */}
         {/* <Route path="/accreditation" element={<Accreditation />} /> */}
